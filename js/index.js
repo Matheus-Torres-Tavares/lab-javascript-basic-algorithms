1c847d2df93ffa35b869402ff6f8b73c70151408
matheus <mtava002@gmail.com>
2020-08-19 14:12:43 -0400

    Finished Bonus exercise 1

--- js/index.js
+++ js/index.js
@@ -48,3 +48,18 @@ for(i=0; i<hacker1.length;i++){
     break;
   }
 }
+//bonus 1:
+
+let latinp = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris vulputate turpis in odio vulputate mollis in a sapien. Duis tincidunt maximus velit, id finibus est finibus at. Curabitur lobortis tempus risus, non scelerisque nibh sagittis a. Vivamus eget consectetur arcu. Phasellus in diam dui. Etiam eget ipsum sed turpis elementum condimentum et ac mauris. Interdum et malesuada fames ac ante ipsum primis in faucibus. Ut id felis diam. Curabitur aliquam neque nec nulla mattis tempus. Integer vitae lorem ut nibh placerat faucibus eu et sem. Integer hendrerit massa vitae sem finibus vulputate. Fusce vitae felis nisl. Pellentesque eget quam eget massa auctor suscipit ac sed quam. Vestibulum congue mi id velit ullamcorper, eget pulvinar libero porttitor. Sed non mauris consectetur, aliquam neque eu, ultrices lacus. Duis varius viverra augue, ut blandit augue molestie id. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Suspendisse blandit tortor ac justo fringilla, nec pulvinar sapien ultricies. Nulla luctus dapibus enim non imperdiet. Nulla eleifend sem turpis, id tristique nulla facilisis et. Curabitur sit amet eros libero. Sed mattis congue tristique. Ut hendrerit semper ipsum et vehicula. In interdum et elit vitae lobortis. Quisque libero nulla, tempus non nunc id, sagittis sollicitudin felis. Proin sit amet nulla nisl. In faucibus eros at nunc scelerisque venenatis. Nullam sit amet lorem est. Aliquam id purus at augue aliquam dignissim. Curabitur finibus ante nisi, et aliquam nunc ultrices vel. Mauris posuere, erat consectetur placerat feugiat, erat massa mattis nisl, nec viverra eros erat quis odio. Phasellus vehicula tincidunt nisl vitae convallis. Fusce ac risus posuere, pulvinar elit egestas, auctor libero. Praesent sed elementum ante. Pellentesque ac lobortis metus, ac suscipit neque. Sed eleifend nisl at tortor iaculis sollicitudin. Aliquam quis sollicitudin magna, vitae faucibus urna. Pellentesque vehicula ultrices tempor."
+
+
+  let wordsplit = latinp.split(" ").length
+//}
+
+console.log(wordsplit)
+
+let etfind= latinp.split(" ").indexOf('et')
+
+console.log(etfind)
+
+//bonus 2:
